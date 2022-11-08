432498bdd65832ce80d5ff1f24c3711c3e92f978
Alex Johnson <alex.johnson293@gmail.com>
2022-11-08 11:38:09 -0800

    refactor Board to allow for full 9 row
    Co-authored-by: Anton Ch <anton3ch@icloud.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,9 +1,17 @@
 // // Each row contains all numbers from 1 to 9 with no repeats.
 // // Each column contains all numbers from 1 to 9 with no repeats.
-export default function Board(row1) {
-  this.row1 = row1;
-}
+export default function Board(rows) {
+  this.row1 = rows;
+  this.row2 = rows;
+  this.row3 = rows;
+  this.row4 = rows;
+  this.row5 = rows;
+  this.row6 = rows;
+  this.row7 = rows;
+  this.row8 = rows;
+  this.row9 = rows;
 
+}
 //   // row 1 check
 //   // multi row check
 //   // column check
