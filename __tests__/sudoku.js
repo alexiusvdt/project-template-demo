5d5dc0f04cc73741e3ae9ddafaf212ab416375c0
Alex Johnson <alex.johnson293@gmail.com>
2022-11-08 11:32:43 -0800

    pass first test for Board creation
    Co-authored-by: Anton Ch <anton3ch@icloud.com>

--- __tests__/sudoku.js
+++ __tests__/sudoku.js
@@ -1,11 +1,12 @@
-import Board from "/.sudoku.js";
+import Board from "./../src/sudoku.js";
 
 describe('Board', () => {
-  const board = new Board();
 
-  test('shall create a Board object with an array as row property')
+  test('shall create a Board object with an array as row property', () => {
+  let row1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const board = new Board(row1);
   expect(board.row1).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
+  });
 
 
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,11 +1,9 @@
 // // Each row contains all numbers from 1 to 9 with no repeats.
 // // Each column contains all numbers from 1 to 9 with no repeats.
+export default function Board(row1) {
+  this.row1 = row1;
+}
 
-// let row1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// export default function Board(row1) {
-//   this.row1 = row1;
-// }
 //   // row 1 check
 //   // multi row check
 //   // column check
