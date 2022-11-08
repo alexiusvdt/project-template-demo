4dd8e55fc3c3d19ce32f283d73b50b4f8da94778
Alex Johnson <alex.johnson293@gmail.com>
2022-11-08 11:36:33 -0800

    add test for full 9 row board
    Co-authored-by: Anton Ch <anton3ch@icloud.com>

--- __tests__/sudoku.js
+++ __tests__/sudoku.js
@@ -2,10 +2,28 @@ import Board from "./../src/sudoku.js";
 
 describe('Board', () => {
 
-  test('shall create a Board object with an array as row property', () => {
-  let row1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const board = new Board(row1);
+  test('shall create a Board object with nine row properties', () => {
+  let rows = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const board = new Board(rows);
+  board.row1 = rows;
+  board.row2 = rows;
+  board.row3 = rows;
+  board.row4 = rows;
+  board.row5 = rows;
+  board.row6 = rows;
+  board.row7 = rows;
+  board.row8 = rows;
+  board.row9 = rows;
+  
   expect(board.row1).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row2).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row3).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row4).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row5).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row6).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row7).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row8).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(board.row9).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
 
